a46f2802c3ebef548b2f61305db7a659c9026def
Andrew Marshall <andrew.marshall@ensono.com>
2025-04-17 16:15:38 +0100

    updated news item

--- src/components/homePage/news.tsx
+++ src/components/homePage/news.tsx
@@ -33,6 +33,26 @@ function News() {
             <h1>latest news</h1>
             <Row xs={1} lg={3} className="g-4 align-items-start">
                 <Col>
+                <Col>
+                    <NewsItem
+                        title="BOWLS BIG WEEKEND"
+                        openingText="Stanningley will be hosting an open day on Sunday 25th May for the Bowls Big Weekend."
+                        mainText="Members will be on hand to show anyone who is interested in playing bowls how to play. No experience or equipment required, we will provide everything you need."
+                        imgSrc={volunteeringImg}
+                        callback={handleNewsExpand2}
+                        expanded={expandNews2}
+                    />
+                <Col>
+                    <NewsItem
+                        title="LADIES TEAM"
+                        openingText="We are entering a team into the Leeds Ladies League for the 2025 season."
+                        mainText="If you are interested in playing then please contact a club member. No experience is necessary."
+                        imgSrc={ladiesImg}
+                        callback={handleNewsExpand3}
+                        expanded={expandNews3}
+                    />
+                </Col>
+                </Col>
                     <NewsItem
                         title="FUNDRAISING"
                         openingText="Stanningley has signed up to EasyFundraising to raise money for the club."
@@ -42,26 +62,6 @@ function News() {
                         imgSrc={fundraisingImg}
                         callback={handleNewsExpand1}
                         expanded={expandNews1}
-                    />
-                </Col>
-                <Col>
-                    <NewsItem
-                        title="GREEN OPENING DATE"
-                        openingText="The green opens on Friday 28th March."
-                        mainText="We'll be having a social in the evening to celebrate the green opening for the new season. Members and non-members are welcome."
-                        imgSrc={volunteeringImg}
-                        callback={handleNewsExpand2}
-                        expanded={expandNews2}
-                    />
-                </Col>
-                <Col>
-                    <NewsItem
-                        title="LADIES TEAM"
-                        openingText="We are entering a team into the Leeds Ladies League for the 2025 season."
-                        mainText="If you are interested in playing then please contact a club member. No experience is necessary."
-                        imgSrc={ladiesImg}
-                        callback={handleNewsExpand3}
-                        expanded={expandNews3}
                     />
                 </Col>
             </Row>
