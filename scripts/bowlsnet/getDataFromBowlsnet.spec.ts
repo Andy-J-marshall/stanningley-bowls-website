2b40ee6facb3e58364becc99fae8f0ed483b269a
Andrew Marshall <andrew.marshall@ensono.com>
2025-06-11 17:54:03 +0100

    added separate file for league info

--- scripts/bowlsnet/getDataFromBowlsnet.spec.ts
+++ scripts/bowlsnet/getDataFromBowlsnet.spec.ts
@@ -6,88 +6,9 @@ import {
     Page,
 } from '@playwright/test';
 import fs from 'fs';
+import { leagues } from './leagueInformation';
 
 const year = new Date().getFullYear(); // Change this to get data from a different year
-
-const leagues = [
-    {
-        day: 'Leeds Monday Combined',
-        url: '/Leeds-MonComb',
-    },
-    {
-        day: 'Leeds Tuesday Vets',
-        url: '/Leeds-TueVets',
-    },
-    {
-        day: 'Leeds Tuesday',
-        url: '/Leeds-Tue',
-    },
-    {
-        day: 'Leeds Half Holiday',
-        url: '/Leeds-Wed',
-    },
-    {
-        day: 'Leeds Thursday Vets',
-        url: '/Leeds-ThuVets',
-    },
-    {
-        day: 'Leeds Saturday',
-        url: '/Leeds-Sat',
-    },
-    {
-        day: 'Leeds Ladies',
-        url: '/LeedsLadies',
-    },
-    {
-        day: 'Mirfield',
-        url: '/Mirfield',
-    },
-    {
-        day: 'Spen Valley',
-        url: '/WestRiding',
-    },
-    {
-        day: 'Ossett and Horbury',
-        url: '/Ossett',
-    },
-    {
-        day: 'AireWharfe Monday',
-        url: '/AW-Mon',
-    },
-    {
-        day: 'AireWharfe Wednesday Pairs',
-        url: '/AW-WedPairs',
-    },
-    {
-        day: 'AireWharfe Vets',
-        url: '/AW-Vets',
-    },
-    // TODO add back in?
-    // {
-    //     day: 'AireWharfe Saturday',
-    //     url: '/AW-Sat',
-    // },
-    {
-        day: 'Bradford Monday',
-        url: '/Bradford-Mon',
-    },
-    {
-        day: 'Bradford Half Holiday',
-        url: '/Bradford-HalfHol',
-    },
-    {
-        day: 'Bradford Vets',
-        url: '/Bradford-Vets',
-    },
-    {
-        day: 'Bradford Saturday',
-        url: '/Bradford-Sat',
-    },
-    {
-        day: 'Tadcaster',
-        url: '/Tadcaster',
-    },
-];
 
 for (const league of leagues) {
     test(`${league.day} Stats`, async () => {
---
+++ scripts/bowlsnet/leagueInformation.ts
@@ -0,0 +1,78 @@
+export const leagues = [
+  {
+    day: "Leeds Monday Combined",
+    url: "/Leeds-MonComb",
+  },
+  {
+    day: "Leeds Tuesday Vets",
+    url: "/Leeds-TueVets",
+  },
+  {
+    day: "Leeds Tuesday",
+    url: "/Leeds-Tue",
+  },
+  {
+    day: "Leeds Half Holiday",
+    url: "/Leeds-Wed",
+  },
+  {
+    day: "Leeds Thursday Vets",
+    url: "/Leeds-ThuVets",
+  },
+  {
+    day: "Leeds Saturday",
+    url: "/Leeds-Sat",
+  },
+  {
+    day: "Leeds Ladies",
+    url: "/LeedsLadies",
+  },
+  {
+    day: "Mirfield",
+    url: "/Mirfield",
+  },
+  {
+    day: "Spen Valley",
+    url: "/WestRiding",
+  },
+  {
+    day: "Ossett and Horbury",
+    url: "/Ossett",
+  },
+  {
+    day: "AireWharfe Monday",
+    url: "/AW-Mon",
+  },
+  {
+    day: "AireWharfe Wednesday Pairs",
+    url: "/AW-WedPairs",
+  },
+  {
+    day: "AireWharfe Vets",
+    url: "/AW-Vets",
+  },
+  // {
+  //   day: "AireWharfe Saturday",
+  //   url: "/AW-Sat",
+  // },
+  {
+    day: "Bradford Monday",
+    url: "/Bradford-Mon",
+  },
+  {
+    day: "Bradford Half Holiday",
+    url: "/Bradford-HalfHol",
+  },
+  {
+    day: "Bradford Vets",
+    url: "/Bradford-Vets",
+  },
+  {
+    day: "Bradford Saturday",
+    url: "/Bradford-Sat",
+  },
+  {
+    day: "Tadcaster",
+    url: "/Tadcaster",
+  },
+];
