108e1635a4993900369cfaad7d7391b861099301
Andrew Marshall <andrew.marshall@thegymgroup.com>
2025-10-01 00:50:13 +0100

    added todo

--- src/pages/teamStats.tsx
+++ src/pages/teamStats.tsx
@@ -26,6 +26,8 @@ function TeamStats(props: TeamStatsProps) {
         window.scrollTo(0, 0);
     });
 
+    // TODO add dividers to team stats and records to split up sections
+
     function returnTeamComponents() {
         return Object.entries(config.historicTeamInfo)
             .map(([teamKey, teamNames]) => {
